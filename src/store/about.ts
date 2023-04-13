07bb7cd6ecae2d1ba8d3185774b0a6bf78d5182f
chanyoungcoding <white1614@naver.com>
2023-04-13 17:20:22 +0900

    typescript

--- src/store/about.ts
+++ src/store/about.ts
@@ -1,6 +1,15 @@
 import { Store } from '../core/heropy'
 
-export default new Store({
+interface State {
+    photo: string
+    name: string
+    email: string
+    blog: string
+    github: string
+    repository: string
+}
+
+export default new Store<State>({
     photo: 'http://',
     name: 'CHAN / ParkChanYoung',
     email: 'thesecon@gmail.com',
